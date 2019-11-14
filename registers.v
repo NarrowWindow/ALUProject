96d354c42d40d0147a6ad8c141f18ae05040e05b
harshalmad <hmadhavapeddi@gmail.com>
2019-11-14 17:30:04 -0600

    Update registers.v

--- registers.v
+++ registers.v
@@ -1,9 +1,10 @@
 module register_a (reset, CLK, D, Q);
+	#(parameter N=16)
 	input reset;
 	input CLK;
-	input [7:0] D;
-	output [7:0] Q;
-	reg [7:0] Q;
+	input [N-1:0] D;
+	output [N-1:0] Q;
+	reg [N-1:0] Q;
 	always @(posedge CLK)
 	if (reset)
 		Q = 0;
@@ -12,11 +13,12 @@ module register_a (reset, CLK, D, Q);
 endmodule // reg8
 
 module register_b (reset, CLK, D, Q);
+	#(parameter N=16)
 	input reset;
 	input CLK;
-	input [7:0] D;
-	output [7:0] Q;
-	reg [7:0] Q;
+	input [N-1:0] D;
+	output [N-1:0] Q;
+	reg [N-1:0] Q;
 	always @(posedge CLK)
 	if (reset)
 		Q = 0;
